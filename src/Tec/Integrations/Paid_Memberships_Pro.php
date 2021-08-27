c3ae3198d363b7bd6f91b3a7093e79df84d51e7a
sky <sky@sky.camp>
2021-08-27 14:59:48 -0500

    Add Paid Memberships Pro integration

--- src/Tec/Integrations/Paid_Memberships_Pro.php
+++ src/Tec/Integrations/Paid_Memberships_Pro.php
@@ -18,6 +18,16 @@ namespace Tribe\Extensions\Membersonlytickets\Integrations;
  */
 class Paid_Memberships_Pro {
 
+
+	/**
+	 * Adds the filters required by the plugin.
+	 *
+	 * @since 1.0.0
+	 */
+	protected function filters() {
+		add_filter( 'tribe_template_context', [ $this, 'filter_ticket_visibility'], 100, 4 );
+	}
+
 	/**
 	 * Check if user can access tickets
 	 *
@@ -36,5 +46,46 @@ class Paid_Memberships_Pro {
 		}
 
 		return pmpro_hasMembershipLevel( $membership_level );
+	}
+
+
+	/**
+	 * Check if user can access tickets
+	 *
+	 * @since 1.0.0
+	 */
+	public function filter_ticket_visibility( $context, $file, $name, $obj ) {
+
+		// bail if not the target template
+		if ( 'v2/tickets' !== implode( "/", $name ) ) {
+			return $context;
+		}
+
+		// The category added to members only products in WooCommerce.
+		$members_only_product_category = tribe( 'extension.members_only_tickets.plugin' )->get_option( 'product_category' );
+
+		// The required membership level.
+		$required_membership_level_name = tribe( 'extension.members_only_tickets.plugin' )->get_option( 'required_membership_level' );
+
+		// If options not set, just carry on.
+		if ( empty( $members_only_product_category ) || empty( $required_membership_level_name ) ) {
+			return $context;
+		}
+
+		// Is user a member?
+		$user_is_member = self::is_member( $required_membership_level_name );
+
+		foreach( $context['tickets'] as $index => $ticket ) {
+			if( ! has_term( $members_only_product_category, 'product_cat', $ticket->ID ) ) continue;
+			if( ! $user_is_member ) {
+
+				$on_sale_index = array_search( $ticket->ID, array_column( $context['tickets_on_sale'], 'ID' ) );
+
+				unset( $context['tickets'][$index] );
+				unset( $context['tickets_on_sale'][$on_sale_index] );
+			}
+		}
+
+		return $context;
 	}
 }
